31401c9d96a890b536ac4b6cf533660e7f5e3a4c
Andrii <andriilukich@gmail.com>
2019-10-03 13:51:34 +0700

    Assignment #10: Countdown

--- project Yoga/js/script.js
+++ project Yoga/js/script.js
@@ -33,4 +33,55 @@ window.addEventListener('DOMContentLoaded', function(){
 			}
 		}
 	});
+
+	// Timer
+
+	let deadline = '2019-12-15';
+
+	function getTimeRemaining(endtime) {
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			seconds = Math.floor((t / 1000) % 60),
+			minutes = Math.floor( (t / (1000 * 60) % 60) ),
+			hours = Math.floor( (t / (1000 * 60 * 60) % 24 ) ),
+			days = Math.floor(t / (1000 * 60 * 60 * 24) );
+
+		return {
+			total : t,
+			seconds,
+			minutes,
+			hours,
+			days
+		};
+	}
+
+	function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+			seconds = timer.querySelector('.seconds'),
+			minutes = timer.querySelector('.minutes'),
+			hours = timer.querySelector('.hours'),
+			days = timer.querySelector('.days'),
+			timeInterval = setInterval(updateClock, 1000);
+
+			if (endtime <= 0) {
+				clearInterval(timeInterval);
+			}
+
+		function updateClock() {
+			let upTime = getTimeRemaining(endtime);
+			seconds.textContent = (upTime.seconds < 10) ? '0' + upTime.seconds : upTime.seconds;
+			minutes.textContent = (upTime.minutes < 10) ? '0' + upTime.minutes : upTime.minutes;
+			hours.textContent = (upTime.hours < 10) ? '0' + upTime.hours : upTime.hours;
+			days.textContent = (upTime.days < 10) ? '0' + upTime.days : upTime.days;
+
+			if (upTime.total <= 0) {
+				clearInterval(timeInterval);
+				seconds.textContent = '00';
+				minutes.textContent = '00';
+				hours.textContent = '00';
+				days.textContent = '00';
+			}
+		}
+	}
+
+	setClock('timer', deadline);
 });
