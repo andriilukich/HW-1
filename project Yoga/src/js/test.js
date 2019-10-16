d3b02e35e86dea74b927bcd78cd66b5e8337bbcb
Andrii <andriilukich@gmail.com>
2019-10-16 10:49:29 +0700

    tests

--- project Yoga/src/js/test.js
+++ project Yoga/src/js/test.js
@@ -1,5 +1,15 @@
 describe('Timer', function() {
     it('Does it give back an object?', function() {
-        assert.typeOf(getTimeRemaining(), 'object');
+        assert.typeOf(getTimeRemaining(endtime), 'object');
+    });
+
+    it('Set the timer', function() {
+        assert.typeOf(timer('timer', '2019-12-15', '.seconds', '.minutes', '.hours', '.days'), 'string');
+    });
+
+    describe('Total sum', function() {
+        it('Total sum', function() {
+            assert.equal(total, 0);
+        });
     });
 });
